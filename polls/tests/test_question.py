ecf92d2a9633c391e7bdfbecec7ce758241a7b48
vnsvakanda <phinyaphak.c@ku.th>
2021-10-26 01:47:21 +0700

    create and move tests directory with an init file and add auth test

--- polls/tests/test_question.py
+++ polls/tests/test_question.py
@@ -3,7 +3,7 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
-from .models import Question
+from polls.models import Question
 from django.urls import reverse
 
 
